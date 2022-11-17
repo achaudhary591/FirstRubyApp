28a30f8603f7c058819099e1b50b9b8850498b52
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-17 13:12:19 +0530

    added article link to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_161344) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_042633) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 end
