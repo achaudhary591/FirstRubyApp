924d234c4b8303984efcd09f458491629b3d313b
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-18 16:11:48 +0530

    newcontrollerlogin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#ndex"
   root "pages#home"
+  # get "login", to: "pages#home"
   get "about", to: "pages#about"
-  resources :articles #, only: [:show]
 end
